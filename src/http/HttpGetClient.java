401c1b173e99319ff6dd63c4a0356332e0e9eb98
dbstjlee <dbstjlee@naver.com>
2024-06-05 17:19:43 +0900

    2024-06-05

--- src/http/HttpGetClient.java
+++ src/http/HttpGetClient.java
@@ -16,7 +16,7 @@ public class HttpGetClient {
 		// HTTP 통신을 하기 위한 준비물
 		// 서버 주소(경로 준비)
 		String urlString = "https://jsonplaceholder.typicode.com/todos/5";
-		// 링크 = 자바 기준으로는 아직 문자열임.
+		// 링크 => 자바 기준으로는 아직 문자열임.
 
 		// 1. URL 클래스를 만들어준다.
 		// 2. Connection 객체를 만들어 준다. (URL --> 멤버로 Connection 객체를 뽑을 수 있다.)
@@ -34,7 +34,7 @@ public class HttpGetClient {
 			int responseCode = conn.getResponseCode();
 			System.out.println("HTTP CODE : " + responseCode);
 			// 200 -> 통신 성공(네이버 서버가 클라이언트에게 응답함)
-			// 401 --> 자원, 프로토콜, 페이지를 찾을 수 없다.
+			// 404 --> 자원, 프로토콜, 페이지를 찾을 수 없다.
 
 			BufferedReader brIn = new BufferedReader(new InputStreamReader(conn.getInputStream())); 
 			// 소켓 기반이라 스트림을 열어서 데이터를 받을 수 있다.
@@ -53,7 +53,6 @@ public class HttpGetClient {
 				System.out.println(word);
 				// System.out.println(); // 한 줄 띄우기
 			}
-
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
